5140fc8a2b031760de5dee3e415cf6813ef56189
uday_kumar_dubey <167726028+Uday2309@users.noreply.github.com>
2025-12-12 23:58:44 +0530

    Delete online reservation system directory

--- online reservation system/src/Utils.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-
-public class Utils {
-    public static String generatePNR() {
-        Random rand = new Random();
-        return "PNR" + (100000 + rand.nextInt(900000));
-    }
-}
