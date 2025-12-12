44f45ccb2ed3e1ead998e6d6f4994cb8a92e9dd2
uday_kumar_dubey <167726028+Uday2309@users.noreply.github.com>
2025-12-12 23:59:59 +0530

    Delete numpy directory

--- numpy/1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-student= np.array([1,2,3,4,2,1,1,3,4,2,1,1,2,3,4,2,1,1])
-
-total=0 
-for i in student:
-    if i==1:
-        total+=1
-print("Total number of students who scored 1:", total)
